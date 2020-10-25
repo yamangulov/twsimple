7e6ed8ef501b3b3eeb9c48f93a0b35018298515d
Iamangulov Andrey <yamangulov@gmail.com>
2020-10-25 22:27:40 +0300

    Creating main page and corresponding methods in controller

--- src/main/java/com/yamangulov/twsimple/GreetingController.java
+++ src/main/java/com/yamangulov/twsimple/GreetingController.java
@@ -1,13 +1,19 @@
 package com.yamangulov.twsimple;
 
+import com.yamangulov.twsimple.domain.Message;
+import com.yamangulov.twsimple.repos.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
@@ -15,4 +21,35 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String tag, Map<String, Object> model) {
+        tag = tag.trim();
+        Iterable<Message> messages;
+        if (tag != null && !tag.isEmpty()) {
+            messages = messageRepository.findByTag(tag);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.put("messages", messages);
+        return "main";
+    }
 }
--- src/main/java/com/yamangulov/twsimple/repos/MessageRepository.java
+++ src/main/java/com/yamangulov/twsimple/repos/MessageRepository.java
@@ -3,5 +3,8 @@ package com.yamangulov.twsimple.repos;
 import com.yamangulov.twsimple.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
 }
